464c53df03275f22c1c2c05281a62c36cc801441
Ashley Kleynhans <ashley.kleynhans@seecrypt.com>
2014-07-17 21:17:21 +0200

    Renamed product images

--- sql/database.sql
+++ sql/database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS comments;
+CREATE DATABASE IF NOT EXISTS comments; 
 GRANT UPDATE,DELETE,SELECT,INSERT,CREATE ON comments.* TO comments@'localhost' IDENTIFIED BY '(0mM3Nt$EnG!n3';
 
 USE comments;
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS categories(
   KEY (updated)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categories (`category_id`, `category_name`, `created`) VALUES(1, 'Abercrombie Fitch', 'AE', NOW());
-INSERT INTO categories (`category_id`, `category_name`, `created`) VALUES(2, 'Hollister', 'HO', NOW());
-INSERT INTO categories (`category_id`, `category_name`, `created`) VALUES(3, 'Ralph Lauren', 'RA', NOW());
+INSERT INTO categories (`category_id`, `category_name`, `category_prefix`, `created`) VALUES(1, 'Abercrombie Fitch', 'AE', NOW());
+INSERT INTO categories (`category_id`, `category_name`, `category_prefix`, `created`) VALUES(2, 'Hollister', 'HO', NOW());
+INSERT INTO categories (`category_id`, `category_name`, `category_prefix`, `created`) VALUES(3, 'Ralph Lauren', 'RA', NOW());
 
 CREATE TABLE IF NOT EXISTS products(
   product_id int(10) unsigned NOT NULL auto_increment,
@@ -31,17 +31,17 @@ CREATE TABLE IF NOT EXISTS products(
   KEY(category_id),
   UNIQUE KEY(product_code),
   KEY (product_price),
-  KEY (producr_stock),
+  KEY (product_stock),
   KEY (created),
   KEY (updated)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(1, 1, 'AE001', 65.00, 10, NOW());
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(2, 1, 'AE002', 65.00, 10, NOW());
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(3, 2, 'HO001', 65.00, 10, NOW());
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(4, 2, 'HO002', 65.00, 10, NOW());
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(5, 3, 'RA001', 65.00, 10, NOW());
-INSERT INTO categories (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(6, 3, 'RA002', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(1, 1, 'AE001', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(2, 1, 'AE002', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(3, 2, 'HO001', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(4, 2, 'HO002', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(5, 3, 'RA001', 65.00, 10, NOW());
+INSERT INTO products (`product_id`, `category_id`, `product_code`, `product_price`, `product_stock`, `created`) VALUES(6, 3, 'RA002', 65.00, 10, NOW());
 
 CREATE TABLE IF NOT EXISTS comments(
   comment_id int(10) unsigned NOT NULL auto_increment,
@@ -60,9 +60,9 @@ CREATE TABLE IF NOT EXISTS comments(
 
 CREATE TABLE IF NOT EXISTS users(
   user_id int(10) unsigned NOT NULL auto_increment,
-  username varchar(20) NOT NULL;
-  password varchar(100) NOT NULL;
-  fullname varchar(30) NOT NULL;
+  username varchar(20) NOT NULL,
+  password varchar(100) NOT NULL,
+  fullname varchar(30) NOT NULL,
   admin tinyint(1) unsigned NOT NULL DEFAULT 0,
   created datetime NOT NULL,
   updated timestamp NOT NULL,
